653c47ced753c0dc182cec2d622e5cc28384f7ea
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-15 23:34:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ function produceDrivingRange(blockRange) {
   return function(start, end) {
     let start = parseInt(start, 10);
     let end = parseInt(end, 10);
+    let distance = Math.abs(start - end)
+    let difference = num - distance
 
-    return drivingCalculator * blockRange;
+    if (difference > 0) {
+      return 'within range of ${difference}'
+    }
+    else {
+      return `${Math.abs(difference)} blocks out of range`
+    }
   };
 }
