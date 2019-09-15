96ffa29f75615d1b0b007b54b9d9436c38fdc049
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-15 23:38:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function produceDrivingRange(blockRange) {
   return function (startBlock, endBlock) {
     let start = parseInt(startBlock);
     let end = parseInt(endBlock);
-    let distance = Math.abs(start - end);
+    let distance = Math.abs(end - start);
     let difference = blockRange - distance;
 
     if (difference > 0) {
