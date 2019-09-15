f71f6372c146a94b287e6769f7e78736d788a7d3
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-15 23:31:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function produceDrivingRange(blockRange) {
   return function(start, end) {
+    let start = parseInt(start, 10);
+    let end = parseInt(end, 10);
+
     return drivingCalculator * blockRange;
   };
 }
